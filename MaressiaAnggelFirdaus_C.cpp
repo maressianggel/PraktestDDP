c103c655fb9866e128421ac5914a3e39256d7cb8
maressianggel <112618457+maressianggel@users.noreply.github.com>
2022-09-07 21:24:00 -1200

    Add files via upload

---
+++ MaressiaAnggelFirdaus_C.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int M , A;
+	
+	cin >> M >> A;
+	
+	cout << M + A << endl;
+	cout << M - A << endl;
+	cout << M * A << endl;
+	cout << M / A << endl;
+	cout << M % A << endl;
+	
+	return 0;
+}
